0e37c3d47d1fbc3b134db2ee66b0779a2f683caf
valerie osawe-edoh <valerieosawedoh@gmail.com>
2022-10-14 04:54:27 -0700

    my first coding

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print the phrase "programming is like building a multilingual puzzle
+ * return: 0 if exited properly, non_zero otherwise
+ */
+
+int main(void)
+{
+	puts ("\"programming is like building a multilingual puzzle");
+	return (0);
+}
+
