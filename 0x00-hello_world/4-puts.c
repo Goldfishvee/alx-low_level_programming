4d930dffae28ba0c7073352fb44d2bad1e820ab8
Valerie Osawe-edoh <113637565+Goldfishvee@users.noreply.github.com>
2022-10-14 13:07:43 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,8 @@
  * main - print the phrase "programming is like building a multilingual puzzle
  * return: 0 if exited properly, non_zero otherwise
  */
-
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
-
