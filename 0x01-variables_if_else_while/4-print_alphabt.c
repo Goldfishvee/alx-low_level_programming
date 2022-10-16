18b1e0ec93c2db0ae09aa968cf45eb3e0fd45bb8
valerie osawe-edoh <valerieosawedoh@gmail.com>
2022-10-16 14:00:56 -0700

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets
+ * alphabets are looped using the while loop
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	char ch = 'a';
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+		if (ch == 'e')
+		{
+			ch++;
+			continue;
+		}
+		if (ch == 'q')
+		{
+			ch++;
+			continue;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
