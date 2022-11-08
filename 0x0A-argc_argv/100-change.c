c25d8ff1e42a359990451b865679ee8c8651a4eb
Valerie Osawe-edoh <113637565+Goldfishvee@users.noreply.github.com>
2022-11-08 23:20:33 +0100

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,42 +2,53 @@
 #include <stdlib.h>
 
 /**
- * main - print minimum number of coins to make change
- * @argc: counter
- * @argv: vector
- * Return: 0 or 1
+ * main - print the minimum number of coins to make change
+ * @argc: argument count
+ * @argv: string vector of argc
+ *
+ * Did not use atoi
+ * because didn't give any flags when there was a conv error
+ *
+ * Return: change, error and 1 if is arg < 2, 0 if no arg provided
  */
-
 int main(int argc, char *argv[])
 {
-	int res, i, j, k, l, m;
+	unsigned int i;
+	int amount, change;
+	int cents[] = {25, 10, 5, 2, 1};
+	char *a;
 
-	i = 25;
-	j = 10;
-	k = 5;
-	l = 2;
-	m = 1;
-
+	/* first condition */
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	entry = atoi(argv[1]);
+	amount = strtol(argv[1], &a, 10); /* change string to int */
+	change = 0;
 
-	if (entry <= 0)
+	if (!*a) /* not int */
+	{
+		while (amount > 1)
+		{
+			for (i = 0; i < sizeof(cents[i]); i++) /* iterate element by element */
+			{
+				if (amount >= cents[i])
+				{
+					change += amount / cents[i]; /* change required */
+					amount = amount % cents[i];
+				}
+			}
+		}
+		if (amount == 1)
+			change++; /* count number of change */
+	}
+	else
 	{
 		printf("0\n");
-		return (0);
 	}
 
-	if (entry >= i && entry % i == 0)
-	{
-		res = entry / i;
-		printf("%d\n", res);
-	}
-	if (entry > i && entry % i != 0)
-	{
-		hold = entry / i;
-		excess = 
+	printf("%d\n", change); /* change change number */
+	return (0);
+}
