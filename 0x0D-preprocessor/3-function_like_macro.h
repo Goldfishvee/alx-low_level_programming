443b98a9267dd8100810af3ccaea71eef974a34d
Valerie Osawe-edoh <113637565+Goldfishvee@users.noreply.github.com>
2022-11-15 22:29:58 +0100

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _3-FUNCTION_LIKE_MACRO_
-#define _3-FUNCTION_LIKE_MACRO_
+#ifndef _3_FUNCTION_LIKE_MACRO_
+#define _3_FUNCTION_LIKE_MACRO_
 
 #define ABS(x) ((x) < 0 ? (-(x)) : (x))
 
-#endif /* _3_FUNCTION_LIKE_MACRO *
+#endif /* _3_FUNCTION_LIKE_MACRO */
